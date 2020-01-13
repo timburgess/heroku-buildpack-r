cb555735935bc289b2f8d2da6f080bfc1712f01d
Ryan Patrick Kyle <ryan@plot.ly>
2020-01-13 11:40:37 -0500

    set host and port; detect run.R and app.R

--- bin/renv.sh
+++ bin/renv.sh
@@ -5,4 +5,6 @@ CHROOT_DIR="$APP_DIR/.root"
 TOOLS_DIR="$APP_DIR/.tools"
 FAKEROOT_DIR="$TOOLS_DIR/fakeroot"
 FAKECHROOT_DIR="$TOOLS_DIR/fakechroot"
+DASH_PORT=5000
+DASH_HOST="0.0.0.0"
 export PATH="$FAKEROOT_DIR/bin:$FAKECHROOT_DIR/sbin:$FAKECHROOT_DIR/bin:./bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
